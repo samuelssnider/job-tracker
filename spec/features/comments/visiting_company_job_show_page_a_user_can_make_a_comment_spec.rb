5da03a698f80fd75404e7aa4de2b512648456759
samuelssnider <sam_redins@yahoo.com>
2017-09-03 10:21:46 -0600

    getting the right error now

---
+++ spec/features/comments/visiting_company_job_show_page_a_user_can_make_a_comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "When visiting a company's job show page" do
+	it "a user can make a comment on that job" do
+		category = Category.create(title: "Sales")
+    company = Company.create!(name: "ESPN")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
+    job2 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category_id: category.id )
+
+		visit company_job_path(company, job1)
+		click_on "Create Comment"
+		fill_in "Content", with: "Damn, that is a sweet job you might say!"
+		click on "Submit Comment"
+
+		expect(page).to have_content("Damn, that is a sweet comment")
+	end
+
+end
